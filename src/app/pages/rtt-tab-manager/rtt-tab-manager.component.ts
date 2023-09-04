b1ed6b2a95c8cfa3ca9fbe20fe8859680f809b59
Abel Ciccoli <abelcoli@outlook.fr>
2023-09-04 16:38:07 +0200

    feat(absenceEmployes): ajout du filtre

--- src/app/pages/rtt-tab-manager/rtt-tab-manager.component.ts
+++ src/app/pages/rtt-tab-manager/rtt-tab-manager.component.ts
@@ -4,6 +4,9 @@ import { TableauButton } from 'src/app/models/tableau-buttons';
 import { RttTabService } from './providers/rtt-tab.service';
 import { AbsenceEmployeur } from 'src/app/models/absence-employeur';
 import { TypeAbsenceEmployeur } from 'src/app/models/type-absence-employeur';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+type FilterFunc = (absence: AbsenceEmployeur) => boolean;
 
 @Component({
   selector: 'app-rtt-tab-manager',
@@ -19,13 +22,21 @@ export class RttTabManagerComponent {
   annee: number = new Date().getFullYear();
   entities: AbsenceEmployeur[] = [];
   absenceSubscription?: Subscription
+  formCheckbox : FormGroup
+  filterFunc: FilterFunc = (absence: AbsenceEmployeur) => true;
 
-  constructor(public service: RttTabService) { }
+
+  constructor(public service: RttTabService, fb:FormBuilder) {
+    this.formCheckbox = fb.group({
+      jf: [true],
+      rtt: [true],
+    })
+  }
 
   ngOnInit(): void {
     this.service.getAbsences(this.annee);
     this.absenceSubscription = this.service.getEntitiesSubject().subscribe(value => {
-      this.entities = value;
+      this.entities = value.filter(this.filterFunc);
     })
   }
 
@@ -45,5 +56,26 @@ export class RttTabManagerComponent {
 
   handleAjout(typeJour : TypeAbsenceEmployeur){
     console.log(typeJour);
+  }
+
+  handleFilter(){
+    if(this.jf.value && this.rtt.value){
+      this.filterFunc = (absence: AbsenceEmployeur) => true;
+    } else if(this.jf.value && !this.rtt.value){
+      this.filterFunc = (absence: AbsenceEmployeur) => absence.type == TypeAbsenceEmployeur.FERIE;
+    } else if(this.rtt.value){
+      this.filterFunc = (absence: AbsenceEmployeur) => absence.type == TypeAbsenceEmployeur.RTT;
+    } else {
+      this.filterFunc = (absence: AbsenceEmployeur) => false;
+    }
+    this.service.getAbsences(this.annee);
+  }
+
+  get jf() {
+    return this.formCheckbox.get('jf') as FormControl;
+  }
+
+  get rtt() {
+    return this.formCheckbox.get('rtt') as FormControl;
   }
 }
