edeac9bd13dc2ac809294e3e72a50f4d3496ee1b
arny-wilk <arnywilk@gmail.com>
2023-09-04 15:06:30 +0200

    feat(creation.absence.component.ts) done

--- src/app/formulaire/components/creation.absence/creation.absence.component.ts
+++ src/app/formulaire/components/creation.absence/creation.absence.component.ts
@@ -1,54 +1,77 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Absence } from 'src/app/models/absence';
+import { Router } from '@angular/router';
 import { StatusAbsence } from 'src/app/models/status-absence';
 import { AbsenceHttpService } from 'src/app/providers/absence-http-service';
+import { TypeConge } from 'src/app/models/type-conge';
 
 @Component({
   selector: 'app-creation.absence',
   templateUrl: './creation.absence.component.html',
   styleUrls: ['./creation.absence.component.scss'],
 })
-export class CreationAbsenceComponent implements OnInit {
+export class CreationAbsenceComponent {
 
   absence!: Absence;
-  
   @Input() dateDebut!: Date;
   @Input() dateFin!: Date;
-  congesPayes: boolean = false;
-  congesSansSolde: boolean = false;
-   rtt: boolean = false;
-   motif: string = "ajouter votre motif si c'est un congé sans solde";
+  typeConge!: TypeConge;
+  motif: string = "ajouter votre motif si c'est un congé sans solde";
+
+  formValid: string = '';
+  formError: string = '';
+  submitted: boolean = false;
   
   form: FormGroup = new FormGroup({
     dateDebut: new FormControl(this.dateDebut),
     dateFin: new FormControl(this.dateFin),
-    congesPayes: new FormControl(this.congesPayes),
-    congesSansSolde: new FormControl(this.congesSansSolde),
-    rtt: new FormControl(this.rtt),
+    typeConge: new FormControl(this.typeConge),
     motif: new FormControl(this.motif),
   });
 
-  constructor(private _absenceHttpService: AbsenceHttpService) {}
-  
-  ngOnInit(): void {
-    this._init()
-    this.onSubmit();
-  }
-
-  private _init() {
-    
+  constructor(private fb: FormBuilder,  private _absenceHttpService: AbsenceHttpService) {
+    this.form = this.fb.group({
+      dateDebut: '',
+      dateFin: '',
+      typeConge: TypeConge.PAYE,
+      motif: '',
+    })
 }
 
   private onSubmit() {
-    this.absence.dateDebut = this.dateDebut;
-    this.absence.dateFin = this.dateFin;
-    this.absence.motif = this.motif;
-    this.absence.status = StatusAbsence.INITIALE;
-    this.absence.typeConge = 
+      this.submitted = true;
+      this.absence.dateDebut = this.form.value.getDateDebut?.value;
+      this.absence.dateFin = this.form.value.getDateFin?.value;
+      this.absence.typeConge = this.form.value.getTypeConge?.value;
+      this.absence.motif = this.form.value.getMotif?.value;
 
+    if(this.absence.typeConge !== TypeConge.SANS_SOLDE) {
+       this.absence.motif = '';
+  }
+
+  if(this.form.valid) {
     this._absenceHttpService.post(this.absence).subscribe( () => {
-      console.log(`Votre demande de congés a bien été prise en compte`);
-})
+      next: () => this.formValid =  'Votre demande de congés a bien été prise en compte';
+      error: (err: { error: { message: string; }; }) => {this.formError = err.error.message;}
+    })
 }
+}
+
+get getDateDebut() {
+  return this.form.get('dateDebut');
+}
+
+get getDateFin() {
+  return this.form.get('dateFin');
+}
+
+get getTypeConge() {
+  return this.form.get('typeConge');
+}
+
+get getMotif() {
+  return this.form.get('motif');
+}
+
 }
