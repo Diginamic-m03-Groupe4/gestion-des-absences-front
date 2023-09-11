a6083fcb902c083528b8c80566d28f1665707d46
Goertz Juliette <129963661+JulietteGTB@users.noreply.github.com>
2023-09-12 00:55:08 +0200

    style navbar, employee modals, login/signup

--- src/app/shared/modal-modif-jf/modal-modif-jf.component.ts
+++ src/app/shared/modal-modif-jf/modal-modif-jf.component.ts
@@ -17,7 +17,7 @@ export class ModalModifJFComponent implements OnInit{
   constructor( @Inject(MAT_DIALOG_DATA) private data: JourFerie, private service: RttTabService, private dialog : MatDialog) {}
 
   ngOnInit(): void {
-    this.messageModif = `Voulez-vous marqué ce jour comme${(this.data.worked) ? " non" : ""} travaillé?`
+    this.messageModif = `Voulez-vous marquer ce jour comme${(this.data.worked) ? " non" : ""} travaillé?`
   }
 
   onModif(){
